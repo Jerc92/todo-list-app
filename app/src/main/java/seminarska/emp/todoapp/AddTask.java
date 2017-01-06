98d9574575819b9b104b653981c369c6ca6b5461
Jernej Barbarič <jernej.barbaric@gmail.com>
2017-01-06 16:57:19 +0100

    Added spinner options.

--- app/src/main/java/seminarska/emp/todoapp/AddTask.java
+++ app/src/main/java/seminarska/emp/todoapp/AddTask.java
@@ -2,12 +2,26 @@ package seminarska.emp.todoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class AddTask extends AppCompatActivity {
+
+    EditText info;
+    Spinner recurringDays;
+    String[] DayOfWeek = {"Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_task);
+
+        info = (EditText) findViewById(R.id.info_editText);
+        recurringDays = (Spinner) findViewById(R.id.repeat_spinner);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, R.layout.weekday, R.id.day_checkbox, DayOfWeek);
+
+        recurringDays.setAdapter(adapter);
     }
 }
